0a39ea386f1a0a765a618a7e345d40aa574fae29
kausikaaa <kauss0806@gmail.com>
2025-09-05 22:23:38 +0530

    added models and config

---
+++ server/models.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const taskSchema = new mongoose.Schema ({
+    title:{
+    type : String,
+    required : [true, "please add a title"],
+},
+    completed:{
+    type:Boolean,
+    default:false,
+    },
+},
+{timestramps : true }
+);
+
+const Task= mongoose.model("Task",taskSchema);
+export default Task;
